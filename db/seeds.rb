e8d2e2416acd4afde14489c46b1ed7b13ba4d8aa
github username <github email address>
2019-02-14 09:42:26 -0500

    trying againg

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+require 'faker'
+
+
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+<<<<<<< .merge_file_h2I6EK
 
 
 style1 = Style.create(name: "Bohemian", season: "Summer")
 style2 = Style.create(name: "Hood Goth", season: "Winter")
 style3 = Style.create(name: "Vintage", season: "Year round")
 style4 = Style.create(name: "Couture", season: "spring")
+=======
+Faker::Avatar.image #=> "https://robohash.org/sitsequiquia.png?size=300x300&set=set1"
+
+Faker::Avatar.image("my-own-slug") #=> "https://robohash.org/my-own-slug.png?size=300x300&set=set1"
+
+Faker::Avatar.image("my-own-slug", "50x50") #=> "https://robohash.org/my-own-slug.png?size=50x50&set=set1"
+
+Faker::Avatar.image("my-own-slug", "50x50", "jpg") #=> "https://robohash.org/my-own-slug.jpg?size=50x50&set=set1"
+
+Faker::Avatar.image("my-own-slug", "50x50", "bmp") #=> "https://robohash.org/my-own-slug.bmp?size=50x50&set=set1"
+
+Faker::Avatar.image("my-own-slug", "50x50", "bmp", "set1", "bg1") #=> "https://robohash.org/my-own-slug.bmp?size=50x50&set=set1&bgset=bg1"
+>>>>>>> .merge_file_l2Jm9k
